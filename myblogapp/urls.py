4e601650e5569265e2a5666663308c58ed4643a5
Kei-ctrl <ishii.night@gmail.com>
2025-01-11 14:19:05 +0900

    Update urls.py

--- myblogapp/urls.py
+++ myblogapp/urls.py
@@ -1,5 +1,5 @@
 import os
-from django.contrib import admin # type: ignore
+from django.contrib import admin # type: .ignore
 from django.urls import include, path # type: ignore
 from django.conf import settings # type: ignore
 from django.conf.urls.static import static # type: ignore
