2929e7c61fe577c3ee27a6c9a80025669a702a4f
Kei-ctrl <ishii.night@gmail.com>
2025-01-11 14:14:32 +0900

    Update serializers.py

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework import serializers # type: ignore
+from rest_framework import serializers # type: .ignore
 from .models import Attraction
 
 class AttractionSerializer(serializers.ModelSerializer):
